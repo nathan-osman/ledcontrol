4699e79da2d88914cde1aa0843334c742dc8b952
Nathan Osman <admin@quickmediasolutions.com>
2013-10-27 19:27:28 -0700

    Added distutils setup.py script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils import setup
+
+setup(name='ledcontrol',
+      version='0.1',
+      description='Classes for controlling LED devices',
+      author='Nathan Osman',
+      author_email='admin@quickmediasolutions.com',
+      url='https://github.com/nathan-osman/ledcontrol',
+      license='MIT',
+      packages=['ledcontrol',])
